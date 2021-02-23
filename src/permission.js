63588fbefdb2028bdef0d6dd7f20404a0cb20258
pursue <996198546@qq.com>
2021-02-23 10:50:50 +0800

    修改验证模式  完成模板开发

--- src/permission.js
+++ src/permission.js
@@ -22,68 +22,30 @@ router.beforeEach(async(to, from, next) => {
   document.title = getPageTitle(to.meta.title)
 
   // determine whether the user has logged in
-  const hasRefreshToken = getRefreshToken()
+  const tokenEffective = await isTokenEffective()
 
-  if (hasRefreshToken) {
+  if (tokenEffective) {
     if (to.path === '/login') {
+      console.log(tokenEffective, 'if is logged in, redirect to the home page')
       // if is logged in, redirect to the home page
       next({ path: '/' })
-      NProgress.done()
+      NProgress.done() // hack: https://github.com/PanJiaChen/vue-element-admin/pull/2939
     } else {
-      next()
-      // if (store.getters.accessTokenExpireUtcTime) {
-      //   // 未超时
-      //   if (
-      //     dayjs
-      //       .utc()
-      //       .add(10, 'minute')
-      //       .isBefore(dayjs(store.getters.accessTokenExpireUtcTime))
-      //   ) {
-      //     next()
-      //   } else if (
-      //     store.getters.refreshTokenExpireTime &&
-      //     dayjs
-      //       .utc()
-      //       .add(29, 'day')
-      //       .isAfter(dayjs(store.getters.refreshTokenExpireTime))
-      //   ) {
-      //     // 超时 直接返回登录页
-      //     store.dispatch('token/logout')
-      //     next(`/login?redirect=${to.path}`)
-      //     NProgress.done()
-      //   } else {
-      //     console.log(getRefreshToken())
-      //     try {
-      //       await store.dispatch('token/refreshToken')
-      //       next()
-      //       NProgress.done()
-      //     } catch (error) {
-      //       // 超时 直接返回登录页
-      //       Message.error(error || 'Has Error')
-      //       await store.dispatch('token/logout')
-      //       next(`/login?redirect=${to.path}`)
-      //       NProgress.done()
-      //     }
-      //   }
-      // }
+      const hasGetUserInfo = store.getters.name
+      if (hasGetUserInfo) {
+        next()
+      } else {
+        try {
+          // get user info
+          await store.dispatch('user/getUserInfo')
 
-      // const hasGetUserInfo = store.getters.name
-      // if (hasGetUserInfo) {
-      //   next()
-      // } else {
-      //   try {
-      //     // get user info
-      //     await store.dispatch('user/getInfo')
-
-      //     next()
-      //   } catch (error) {
-      //     // remove token and go to login page to re-login
-      //     await store.dispatch('user/resetToken')
-      //     Message.error(error || 'Has Error')
-      //     next(`/login?redirect=${to.path}`)
-      //     NProgress.done()
-      //   }
-      // }
+          next()
+        } catch (error) {
+          Message.error(error || 'Has Error')
+          next(`/404`)
+          NProgress.done()
+        }
+      }
     }
   } else {
     /* has no token*/
@@ -103,3 +65,47 @@ router.afterEach(() => {
   // finish progress bar
   NProgress.done()
 })
+
+async function isTokenEffective() {
+  const hasRefreshToken = getRefreshToken()
+  // 无RefreshToken直接返回 false
+  if (!hasRefreshToken) {
+    return false
+  }
+
+  // vuex 无数据直接返回 false
+  if (!store.getters.accessToken) {
+    return false
+  }
+
+  // 正常情况
+  if (
+    dayjs
+      .utc()
+      .add(10, 'minute')
+      .isBefore(dayjs(store.getters.accessTokenExpireUtcTime))
+  ) {
+    return true
+  }
+
+  // RefreshToken 过期
+  if (
+    store.getters.refreshTokenExpireTime &&
+    dayjs
+      .utc()
+      .add(29, 'day')
+      .isAfter(dayjs(store.getters.refreshTokenExpireTime))
+  ) {
+    return false
+  }
+
+  // 尝试获取新token
+  try {
+    await store.dispatch('token/refreshToken')
+    return true
+  } catch (error) {
+    // 超时 直接返回登录页
+    Message.error(error || 'Has Error')
+    return false
+  }
+}
